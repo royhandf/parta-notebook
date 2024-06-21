b30a4af2dd4fb44fef2094cd196cc143f48ad722
royhandf <royhandaffa14@gmail.com>
2024-06-21 09:26:16 +0700

    feat: update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,22 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\Home;
+use App\Controllers\AuthController;
+use App\Controllers\DashboardController;
+use App\Controllers\CategoryController;
+use App\Controllers\ProductController;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', [Home::class, 'index']);
+$routes->get('/login', [AuthController::class, 'index']);
+
+$routes->get('/register', [AuthController::class, 'register']);
+
+$routes->get('/cart', [Home::class, 'cart']);
+
+$routes->get('/dashboard', [DashboardController::class, 'index']);
+$routes->get('/dashboard/categories', [CategoryController::class, 'index']);
+$routes->get('/dashboard/products', [ProductController::class, 'index']);
